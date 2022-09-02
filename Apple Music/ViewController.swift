2ff830c1d9bcc0bb17d7b8fb8677f8260c2ce2e3
Pavel Koyushev <pkoyushev@gmail.com>
2022-09-02 08:49:48 +0300

    Initial Commit

---
+++ Apple Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Music
+//
+//  Created by Pavel Koyushev on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
